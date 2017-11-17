abce0bdb560941daf6f49657aa29a16a2e0e0ab0
nirmalwsolus <nirmal@wsolus.com>
2017-11-18 04:22:54 +0530

    Updated layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Notifys from 'react-notifys';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+          <h1>React Notifys</h1>
+          <p className="discription">Notifys is a React component which provides notifications with many configuration possibilities.</p>
+        <p>
+          <Notifys className="notifys notifys-success fade"><strong>Success!</strong> Indicates a successful or positive action.</Notifys>
         </p>
+          <p>
+              <Notifys className="notifys notifys-info fade"><strong>Info!</strong> Indicates a neutral informative change or action.</Notifys>
+        </p><p>
+          <Notifys className="notifys notifys-warning fade"><strong>Warning!</strong> Indicates a neutral informative change or action.</Notifys>
+        </p>
+
+          <p>
+              <Notifys className="notifys notifys-danger fade"><strong>Danger!</strong> Indicates a neutral informative change or action.</Notifys>
+          </p>
       </div>
     );
   }
