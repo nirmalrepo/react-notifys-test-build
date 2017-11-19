0ee309d5d4263e2cff8e0f9543b87aed2042de92
nirmalwsolus <nirmal@wsolus.com>
2017-11-19 17:21:26 +0530

    Updated to notifys v1.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Notifys from 'react-notifys';
 
 class App extends Component {
-  render() {
+    doSomething = e =>{
+        alert("Hey! You can do more.");
+    };
+
+    render() {
     return (
       <div className="container">
           <h1>React Notifys</h1>
           <p className="discription">Notifys is a React component which provides notifications with many configuration possibilities.</p>
         <p>
-          <Notifys className="notifys notifys-success fade"><strong>Success!</strong> Indicates a successful or positive action.</Notifys>
+          <Notifys className="notifys notifys-success fade" ><strong>Success!</strong> notify a successful or positive action.</Notifys>
         </p>
           <p>
-              <Notifys className="notifys notifys-info fade"><strong>Info!</strong> Indicates a neutral informative change or action.</Notifys>
+              <Notifys className="notifys notifys-info fade"><strong>Info!</strong> notify a neutral informative change or action.</Notifys>
         </p><p>
-          <Notifys className="notifys notifys-warning fade"><strong>Warning!</strong> Indicates a neutral informative change or action.</Notifys>
+          <Notifys className="notifys notifys-warning fade"><strong>Warning!</strong> notify a warning that might need attention.</Notifys>
         </p>
-
           <p>
-              <Notifys className="notifys notifys-danger fade"><strong>Danger!</strong> Indicates a neutral informative change or action.</Notifys>
+              <Notifys className="notifys notifys-danger fade" onClose={this.doSomething}><strong>Danger!</strong> notify a unsuccessful change or action.</Notifys>
           </p>
       </div>
     );
